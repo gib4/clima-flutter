543dad9828679e83eda4757a8f49842b3ba3fe11
Denis Škerk <w44crt@yandex.com>
2023-02-21 09:44:42 +0000

    Final Commit

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,14 @@ const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 const apiKey = '820102d5941d8f15ed7815fd1f03c88c';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        "$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric");
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
